7a47610f861029991cfaca658e7eb4eab9fb1fac
AbdTL <abdalaziztll992010@gmail.com>
2023-04-05 21:08:48 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:norea_school_student/Opening_Screen.dart';
@@ -28,7 +27,6 @@ class MyApp extends StatelessWidget {
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-  
 
   @override
   Widget build(BuildContext context) {
